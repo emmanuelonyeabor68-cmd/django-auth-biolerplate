2e2abe4719ad2f7599a4bf66a454112f17027169
Dev Emmy <emmanuelonyeabor68@gmail.com>
2026-07-08 02:20:25 -0700

    update_site management

---
+++ auth/account/management/commands/update_site.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.sites.models import Site
+import os
+
+class Command(BaseCommand):
+    help = 'Update site domain'
+
+    def handle(self, *args, **kwargs):
+        domain = os.environ.get("FRONTEND_URL", '127.0.0.1:3000')
+        name = os.environ.get('SITE_NAME', 'Auth App')
+        site = Site.objects.get(id=1)
+        site.domain = name
+        site.name = name
+        site.save()
+        self.stdout.writes(f'Site updated tp {domain}')
