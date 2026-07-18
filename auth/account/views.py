0f37f4e334fb38c5050e214d3ce66682a497f4d1
Dev Emmy <emmanuelonyeabor68@gmail.com>
2026-07-18 00:18:33 -0700

    update

--- auth/account/views.py
+++ auth/account/views.py
@@ -5,8 +5,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
